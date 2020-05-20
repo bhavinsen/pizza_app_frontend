bb2bcfa851d6c739dc82fa4530240a78eecb807d
Bhavin Sen <senbhavin@gmail.com>
2020-05-20 17:50:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ const App = () => (
    <BrowserRouter>
        <div className="App">
            <Header user={authService.getCurrentUser()} />
-
            <Switch>
                <Route exact path="/">
                  <HomePage />
